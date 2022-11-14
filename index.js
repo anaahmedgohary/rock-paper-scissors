f455db6636c8cfcdee4eaf0cad681fab482568f0
anaahmedgohary <gogoahmed13@gmail.com>
2022-11-14 17:08:52 +0200

    add express

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ db.connect((err) =>
 
 const app = express();
 
+// port .env for others
+const port = process.env.port || 3002;
+
 // linking the puplic html file
 app.use(express.static('public'))
 
@@ -36,6 +39,38 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+// handle form GET/POST request
+app.post('/add4form', async (req, res) =>
+{
+   // res.json(req.body);
+    const body = req.body;
+    let username = body.username;
+    let password = body.password;
+
+
+    let post = { username: `${username}`, password: `${password}` };
+    let sql = `INSERT INTO tester01 SET ?`;
+
+    let query = db.query(sql, post, (err, result) =>
+    {
+        if (err) throw err;
+        console.log(result);
+       // res.send('input into tabel tester01 24');
+        res.sendFile(__dirname+'/public/thanks.html')
+    })
+
+    // await db.execute(`
+    //   INSERT INTO tester01(username, password) VALUES (
+    //     @username, @password)`, {
+    //     username: body.username,
+    //     password: body.password,
+    // });
+
+   // res.end("yeas")
+})
+
+
 
 
 // create DataBase
@@ -54,7 +89,7 @@ app.get('/createdb', (req, res) =>
 // create table 
 app.get('/createtable', (req, res) =>
 {
-    let sql = `CREATE TABLE tester01(id int AUTO_INCREMENT, username VARCHAR(255) NOT NULL, passsword VARCHAR(255), date VARCHAR(255), PRIMARY KEY (id), UNIQUE dateof (date))`;
+    let sql = `CREATE TABLE tester01(id int AUTO_INCREMENT, username VARCHAR(255) NOT NULL, password VARCHAR(255), date VARCHAR(255), PRIMARY KEY (id), UNIQUE dateof (date))`;
 
     db.query(sql, (err, result) =>
     {
@@ -67,9 +102,9 @@ app.get('/createtable', (req, res) =>
 // insert data
 app.get('/add1post', (req, res) =>
 {
-    let post = { username: 'nomberjuan', passsword: 'numberjtwo' };
+    let post = { username: 'nomberjuan', password: 'numberjtwo' };
     let sql = `INSERT INTO tester01 SET ?`;
-    // (username, password) VALUES('${username}', '${passsword}' )`;
+    // (username, password) VALUES('${username}', '${password}' )`;
 
     let query = db.query(sql, post, (err, result) =>
     {
